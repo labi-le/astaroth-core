31a3b49ac6448acc7e7498b6cbd1bad3c9b6df8e
labile <1a6i1e@gmail.com>
2021-06-28 16:12:14 +0400

    fix repeated requests for callback

--- src/Astaroth/Bootstrap/BotInstance.php
+++ src/Astaroth/Bootstrap/BotInstance.php
@@ -30,7 +30,14 @@ class BotInstance
     private function callback(ContainerBuilder $container): CallBack
     {
         $SECRET_KEY = $container->getParameter("SECRET_KEY");
-        $HANDLE_REPEATED_REQUESTS = (bool)$container->getParameter("HANDLE_REPEATED_REQUESTS");
+
+        $_RAW_HANDLE_REPEATED_REQUESTS = $container->getParameter("HANDLE_REPEATED_REQUESTS");
+
+        if (empty($_RAW_HANDLE_REPEATED_REQUESTS) || $_RAW_HANDLE_REPEATED_REQUESTS === "true") {
+            $HANDLE_REPEATED_REQUESTS = true;
+        } else {
+            $HANDLE_REPEATED_REQUESTS = false;
+        }
 
         return new CallBack(
             $container->getParameter("CONFIRMATION_KEY"),
