f58721b22f2506165860100941e22ef17e05d187
labile <1a6i1e@gmail.com>
2021-06-28 14:09:17 +0400

    добавлена возможность менять токен в фасадах - загрузчиках

--- src/Astaroth/Support/Facades/Message/MessageUploaderFacade.php
+++ src/Astaroth/Support/Facades/Message/MessageUploaderFacade.php
@@ -15,8 +15,18 @@ use Astaroth\VkUtils\Contracts\IVideo;
  */
 class MessageUploaderFacade extends \Astaroth\Support\Facades\Facade
 {
+    private static string $access_token;
+
     public static function upload(IDocsUpload|IVideo|IPhoto|IStories ...$object): array
     {
-        return static::getObject(\Astaroth\VkUtils\Uploading\MessagesUploader::class)->upload(...$object);
+        $uploader = static::getObject(\Astaroth\VkUtils\Uploading\MessagesUploader::class);
+        self::$access_token === null ?: $uploader->setDefaultToken(self::$access_token);
+
+        return $uploader->upload(...$object);
+    }
+
+    public static function changeToken(string $access_token): void
+    {
+        self::$access_token = $access_token;
     }
 }
--- src/Astaroth/Support/Facades/Wall/WallUploaderFacade.php
+++ src/Astaroth/Support/Facades/Wall/WallUploaderFacade.php
@@ -15,8 +15,19 @@ use Astaroth\VkUtils\Contracts\IVideo;
  */
 class WallUploaderFacade extends \Astaroth\Support\Facades\Facade
 {
+    private static string $access_token;
+
     public static function upload(IDocsUpload|IVideo|IPhoto|IStories ...$object): array
     {
-        return static::getObject(\Astaroth\VkUtils\Uploading\WallUploader::class)->upload(...$object);
+        $uploader = static::getObject(\Astaroth\VkUtils\Uploading\WallUploader::class);
+
+        self::$access_token === null ?: $uploader->setDefaultToken(self::$access_token);
+
+        return $uploader->upload(...$object);
+    }
+    
+    public static function changeToken(string $access_token): void
+    {
+        self::$access_token = $access_token;
     }
 }
