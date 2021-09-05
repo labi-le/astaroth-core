496b8f1ccc7fe01a057a549b3f87beee28476258
labile <1a6i1e@gmail.com>
2021-09-05 23:26:26 +0300

    invocable

--- src/Astaroth/Support/Facades/Entity.php
+++ src/Astaroth/Support/Facades/Entity.php
@@ -6,21 +6,27 @@ namespace Astaroth\Support\Facades;
 
 use Astaroth\Containers\DatabaseContainer;
 use Astaroth\Foundation\FacadePlaceholder;
+use Doctrine\ORM\Decorator\EntityManagerDecorator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-final class Entity
+final class Entity extends EntityManagerDecorator
 {
     /**
      * @return ?EntityManagerInterface
      */
-    public static function manage(): ?object
+    private static function getContainer(): ?object
     {
         return FacadePlaceholder::getContainer()->get(DatabaseContainer::CONTAINER_ID, ContainerInterface::NULL_ON_INVALID_REFERENCE);
     }
 
-    public function __invoke()
+    public function __invoke(): Entity
     {
-        return self::manage();
+        return new Entity;
+    }
+
+    public function __construct()
+    {
+        parent::__construct(self::getContainer());
     }
 }
