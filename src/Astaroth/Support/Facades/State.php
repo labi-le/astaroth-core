2b3a92f01ae169db47a4cf2ddab250404a234a48
labile <1a6i1e@gmail.com>
2021-09-27 20:31:06 +0300

    add return typo (cs)

--- src/Astaroth/Support/Facades/State.php
+++ src/Astaroth/Support/Facades/State.php
@@ -22,12 +22,12 @@ final class State
         self::_add($member_id, $state_name);
     }
 
-    public static function add(int $member_id, string $state_name)
+    public static function add(int $member_id, string $state_name): bool
     {
         return self::_add($member_id, $state_name);
     }
 
-    public static function remove(int $member_id, string $state_name)
+    public static function remove(int $member_id, string $state_name): bool
     {
         return (new Session($member_id, \Astaroth\Attribute\State::RESERVED_NAME))
             ->removeKey($state_name);
