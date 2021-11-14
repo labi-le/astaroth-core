03c9f67a682f26f5001c7cb504d10b1b8627bd0d
labile <1a6i1e@gmail.com>
2021-11-14 13:41:53 +0300

    implement AttributeValidatorInterface

--- src/Astaroth/Attribute/Debug.php
+++ src/Astaroth/Attribute/Debug.php
@@ -5,17 +5,16 @@ declare(strict_types=1);
 namespace Astaroth\Attribute;
 
 use Astaroth\Contracts\AttributeReturnInterface;
+use Astaroth\Contracts\AttributeValidatorInterface;
 use Astaroth\Foundation\Utils;
-use Astaroth\TextMatcher;
 use Attribute;
-use JetBrains\PhpStorm\ExpectedValues;
 
 #[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 /**
  * Attribute that always fires
  * It will be useful for dump
  */
-final class Debug implements AttributeReturnInterface
+final class Debug implements AttributeReturnInterface, AttributeValidatorInterface
 {
     private mixed $haystack;
 
