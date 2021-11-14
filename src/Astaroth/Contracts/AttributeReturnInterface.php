15fe38d437dd2acb56a9174d660640e2eae520fa
labile <1a6i1e@gmail.com>
2021-11-14 13:20:07 +0300

    add AttributeReturnInterface

---
+++ src/Astaroth/Contracts/AttributeReturnInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Astaroth\Contracts;
+
+/**
+ * Return result from attribute
+ * @package Astaroth\Contracts
+ */
+interface AttributeReturnInterface
+{
+    public function getResult(): mixed;
+}
