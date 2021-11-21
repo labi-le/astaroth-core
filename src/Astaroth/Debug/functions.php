f92aed04a10b1a9e2522a66ab02da955110c487a
labile <1a6i1e@gmail.com>
2021-11-21 14:11:22 +0300

    remove inspections

--- src/Astaroth/Debug/functions.php
+++ src/Astaroth/Debug/functions.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 namespace Astaroth\Debug;
 
-use function function_exists;
-
-if (!function_exists("dump")) {
-    function dump(mixed ...$mixed): Dump
-    {
-        return new Dump($mixed);
-    }
+function dump(mixed ...$mixed): Dump
+{
+    return new Dump($mixed);
 }
 
-
-if (!function_exists("memory_stat")) {
-    function memory_stat(callable $app, string $convertTo = "M"): Memory
-    {
-        return new Memory($app, $convertTo);
-    }
+function memory_stat(callable $app, string $convertTo = "M"): Memory
+{
+    return new Memory($app, $convertTo);
 }
