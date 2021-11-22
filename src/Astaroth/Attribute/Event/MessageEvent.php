f90c52e5f9e886c6ce9795503068cda19d844583
labile <1a6i1e@gmail.com>
2021-11-22 22:29:57 +0300

    refactor

--- src/Astaroth/Attribute/Event/MessageEvent.php
+++ src/Astaroth/Attribute/Event/MessageEvent.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Astaroth\Attribute\Event;
 
 use Astaroth\Contracts\AttributeValidatorInterface;
+use Astaroth\DataFetcher\DataFetcher;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_CLASS)]
@@ -20,9 +21,13 @@ class MessageEvent implements AttributeValidatorInterface
         return $this->type === "message_event";
     }
 
+    /**
+     * @param DataFetcher $haystack
+     * @return $this
+     */
     public function setHaystack($haystack): static
     {
-        $this->type = $haystack;
+        $this->type = $haystack->getType();
         return $this;
     }
 }
