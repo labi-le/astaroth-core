36a2ce371bd56200af8af0cd91d405f6b22d5db6
labile <1a6i1e@gmail.com>
2021-12-04 13:50:32 +0300

    fix psalm

--- src/Astaroth/Attribute/Payload.php
+++ src/Astaroth/Attribute/Payload.php
@@ -45,6 +45,13 @@ final class Payload implements AttributeValidatorInterface
         return false;
     }
 
+    /**
+     * @param array|string $payload
+     * @param array $haystack
+     * @return bool
+     *
+     * @psalm-suppress PossiblyInvalidArgument
+     */
     private function containsValidate(array|string $payload, array $haystack): bool
     {
         return array_intersect($payload, $haystack) !== [];
