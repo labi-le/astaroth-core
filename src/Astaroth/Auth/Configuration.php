04ee748989315fb3709475f7c5b7633d18099a80
labile <1a6i1e@gmail.com>
2021-12-05 17:16:41 +0300

    refactor Configuration::getCallbackConfirmationKey

--- src/Astaroth/Auth/Configuration.php
+++ src/Astaroth/Auth/Configuration.php
@@ -249,12 +249,7 @@ final class Configuration
      */
     public function getCallbackConfirmationKey(): string
     {
-        $key = $this->getConfig(self::CONFIRMATION_KEY);
-        if (empty($key)) {
-            return throw new ParameterMissingException("Missing parameter " . self::CONFIRMATION_KEY . " from environment");
-        }
-
-        return $key;
+        return $this->getConfig(self::CONFIRMATION_KEY);
     }
 
     public function getDatabaseDriver(): ?string
