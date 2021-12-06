5a55fd138cb0e01ecda5d2c5eefc23c21ea957c2
labile <1a6i1e@gmail.com>
2021-12-06 19:28:02 +0300

    fix test

--- tests/Attribute/PayloadTest.php
+++ tests/Attribute/PayloadTest.php
@@ -15,13 +15,13 @@ class PayloadTest extends TestCase
     {
         $payload = new Payload("bar", Payload::KEY_EXIST);
 
-        assertEquals(Payload::class, $payload->setHaystack(["foo"])::class);
+        assertEquals(Payload::class, $payload->setHaystack((require __DIR__ . "/data.php")->messageNew())::class);
     }
 
     public function testValidate()
     {
         $payload = new Payload("foo", Payload::KEY_EXIST);
-        $payload->setHaystack(["foo" => "bar"]);
+        $payload->setHaystack((require __DIR__ . "/data.php")->messageNew());
         assertTrue($payload->validate());
     }
 }
