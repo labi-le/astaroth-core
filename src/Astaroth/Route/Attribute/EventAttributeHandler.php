1cefde61b31a743687bb9be2e9abba5fb516d45a
labile <1a6i1e@gmail.com>
2021-12-06 13:47:32 +0300

    Validation of class attributes if explicitly indicated

--- src/Astaroth/Route/Attribute/EventAttributeHandler.php
+++ src/Astaroth/Route/Attribute/EventAttributeHandler.php
@@ -30,27 +30,19 @@ class EventAttributeHandler
             /** @psalm-suppress ArgumentTypeCoercion */
             $reflectionClass = new ReflectionClass($class);
 
-            if (
-                (
-                    $this->eventAttrValidate($reflectionClass, $data) ||
-                    $this->customOptionalAttrValidate($reflectionClass, $data)
-                ) === false
-            ) {
+            if ($this->classValidateAttr($reflectionClass, $data) === false) {
                 break;
             }
 
             new EventDispatcher($reflectionClass, $data);
         }
+
     }
 
-    /**
-     * @param ReflectionAttribute[] $reflectionAttributes
-     */
-    private function validateAttribute(array $reflectionAttributes, DataFetcher $data): ?bool
+    private function classValidateAttr(ReflectionClass $reflectionClass, DataFetcher $data): bool
     {
-        $validate = null;
-
-        foreach ($reflectionAttributes as $reflectionAttribute) {
+        $validate = false;
+        foreach ($reflectionClass->getAttributes() as $reflectionAttribute) {
             $attribute = $reflectionAttribute->newInstance();
 
             if ($attribute instanceof AttributeValidatorInterface) {
@@ -63,38 +55,5 @@ class EventAttributeHandler
         }
 
         return $validate;
-    }
-
-    /**
-     * @param ReflectionClass $reflectionClass
-     * @param DataFetcher $data
-     * @return bool
-     *
-     * @see Conversation, State
-     *
-     * @psalm-suppress InvalidArgument
-     */
-    private function eventAttrValidate(ReflectionClass $reflectionClass, DataFetcher $data): bool
-    {
-        return
-            $this->validateAttribute($reflectionClass->getAttributes(MessageNew::class), $data)
-            ?? $this->validateAttribute($reflectionClass->getAttributes(MessageEvent::class), $data)
-            ?? false;
-    }
-
-    /**
-     * @param ReflectionClass $reflectionClass
-     * @param DataFetcher $data
-     * @return bool|null
-     *
-     * @psalm-suppress InvalidArgument
-     */
-    private function customOptionalAttrValidate(ReflectionClass $reflectionClass, DataFetcher $data): ?bool
-    {
-        ///more attr...
-        return
-            $this->validateAttribute($reflectionClass->getAttributes(Conversation::class), $data)
-            ?? $this->validateAttribute($reflectionClass->getAttributes(State::class), $data)
-            ?? true;
     }
 }
