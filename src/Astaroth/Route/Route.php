f29e7ae1e16dbe6f5e440917280cd52130453c29
labile <1a6i1e@gmail.com>
2021-12-09 18:46:45 +0300

    method Route::attributeRouter

--- src/Astaroth/Route/Route.php
+++ src/Astaroth/Route/Route.php
@@ -9,6 +9,7 @@ namespace Astaroth\Route;
 use Astaroth\Contracts\HandlerInterface;
 use Astaroth\DataFetcher\DataFetcher;
 use Astaroth\Route\Attribute\EventAttributeHandler;
+use Astaroth\Route\Attribute\Executor;
 use Exception;
 use HaydenPierce\ClassFinder\ClassFinder;
 use Throwable;
@@ -19,20 +20,12 @@ use Throwable;
  */
 class Route
 {
-    private array $class_map;
+    private static array $class_map;
 
     public function __construct(private HandlerInterface $handler)
     {
     }
 
-    /**
-     * Get class map
-     * @return array
-     */
-    private function getClassMap(): array
-    {
-        return $this->class_map;
-    }
 
     /**
      * Set class map
@@ -42,18 +35,54 @@ class Route
      */
     public function setClassMap(string $class_map): static
     {
-        $this->class_map = ClassFinder::getClassesInNamespace($class_map, ClassFinder::RECURSIVE_MODE);
+        self::$class_map = ClassFinder::getClassesInNamespace($class_map, ClassFinder::RECURSIVE_MODE);
         return $this;
     }
 
     /**
      * Routing data from VK
      * @throws Throwable
+     *
+     * @psalm-suppress PossiblyNullArgument
      */
     public function handle(): void
     {
-        $this->handler->listen(function (DataFetcher $data) {
-            new EventAttributeHandler($this->getClassMap(), $data);
+        $this->handler->listen(static function (DataFetcher $data) {
+            self::attributeRouter(self::$class_map, $data);
+
+            /**
+             * маловероятно что я это сделаю, но как идею нужно записать
+             * TODO: помимо аттрибутов добавить обработку маршрутов по конфигурационному файлу, примерно как в Laravel (api.php)
+             *
+             * TODO: реализовать конфиг роутов
+             *
+             * draft
+             * message === "hi" -> Message::HelloCommand()
+             * payload["key" => "val", Payload::STRICT] -> Keyboard::ButtonPress
+             * etc...
+             */
+
         });
+    }
+
+    /**
+     * @param array $classMap
+     * @param DataFetcher $data
+     * @return void
+     * @throws \ReflectionException
+     *
+     * @psalm-suppress PossiblyNullArgument
+     */
+    private static function attributeRouter(array $classMap, DataFetcher $data): void
+    {
+        $attrHandler = new EventAttributeHandler($classMap, $data);
+
+        foreach ($attrHandler->validate() as $validatedObject) {
+            (new Executor($validatedObject->getObject(), $validatedObject->getMethods()))
+                ->replaceObjects(EventAttributeHandler::fetchData($data))
+                ->launch(static function ($methodResult) {
+                    new ReturnResultHandler($methodResult);
+                });
+        }
     }
 }
