a6676e48914373b2623d553d1fcc9816e2e88451
labile <1a6i1e@gmail.com>
2021-12-09 23:51:31 +0300

    refactor placeholder

--- tests/Foundation/PlaceholderTest.php
+++ tests/Foundation/PlaceholderTest.php
@@ -10,19 +10,52 @@ use function PHPUnit\Framework\assertEquals;
 
 class PlaceholderTest extends TestCase
 {
+    public const LAST_NAME = "last_name";
+    public const FIRST_NAME = "first_name";
 
     /**
+     * Class with which you can add placeholders to messages
+     *
      * @throws \Throwable
+     *
+     * @example FirstName - %name
+     * @example Name with mention "%@name"
+     * @example FirstName LastName "%full-name"
+     * @example FirstName LastName mention "%@full-name"
+     * @example LastName "%last-name"
+     * @example LastName mention "%@last-name"
+     * @example hi %@name
      */
     public function testReplace(): void
     {
-        $mock = $this
-            ->createMock(Placeholder::class);
+        $p = new Placeholder("%name");
+        assertEquals("Павел", $p->replace(1));
+        assertEquals("ВКонтакте API", $p->replace(-1));
 
-        $mock
-            ->method("replace")
-            ->willReturn("привет Павел");
 
-        assertEquals("привет Павел", $mock->replace(1));
+        $p = new Placeholder("%@name");
+        assertEquals("*id1(Павел)", $p->replace(1));
+        assertEquals("*club1(ВКонтакте API)", $p->replace(-1));
+
+
+        $p = new Placeholder("%full-name");
+        assertEquals("Павел Дуров", $p->replace(1));
+        assertEquals("ВКонтакте API", $p->replace(-1));
+
+
+        $p = new Placeholder("%@full-name");
+        assertEquals("*id1(Павел Дуров)", $p->replace(1));
+        assertEquals("*club1(ВКонтакте API)", $p->replace(-1));
+
+
+        $p = new Placeholder("%last-name");
+        assertEquals("Дуров", $p->replace(1));
+        assertEquals("ВКонтакте API", $p->replace(-1));
+
+
+        $p = new Placeholder("%@last-name");
+        assertEquals("*id1(Дуров)", $p->replace(1));
+        assertEquals("*club1(ВКонтакте API)", $p->replace(-1));
+
     }
 }
