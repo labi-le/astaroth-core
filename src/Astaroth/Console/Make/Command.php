3ba0eb3daa31eae60ee57d889f19c5ef7d8f9fb5
labile <1a6i1e@gmail.com>
2021-12-24 02:04:34 +0300

    add cli make:command

---
+++ src/Astaroth/Console/Make/Command.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Astaroth\Console\Make;
+
+use Ahc\Cli\Input\Command as CliCommand;
+use Astaroth\Foundation\Application;
+use Astaroth\Generators\EventGenerator;
+use RuntimeException;
+
+final class Command extends CliCommand
+{
+    public function __construct()
+    {
+        parent::__construct("make:command", "generate command");
+
+
+        $this
+            ->argument('className', 'name of the class that will be indicated when creating')
+            ->argument('event', 'event which class should handle')
+            ->usage(
+                '<bold>  $0</end> <green>make:command</end> Greetings message_new</end><eol/>'
+            )
+            ->action(function (string $className, string $event) {
+                $path = str_replace('\\', DIRECTORY_SEPARATOR, Application::$configuration->getAppNamespace());
+                if (@!mkdir($path, 0777, true) && !is_dir($path)) {
+                    throw new RuntimeException(sprintf('Directory "%s" was not created', $path));
+                }
+
+                file_put_contents(
+                    sprintf('%s%s%s.php', $path, DIRECTORY_SEPARATOR, $className),
+                    EventGenerator::generate(Application::$configuration->getAppNamespace(), $className, $event)
+                );
+            });
+    }
+
+
+}
